376d97bf80e7721591420b735deb4cbaa23be7a1
Wendesday36 <bertazoeanna@gmail.com>
2023-09-13 13:44:47 +0200

    pin kod szamok keverese kezdes,formazas

--- src/pinkod/PinKod.java
+++ src/pinkod/PinKod.java
@@ -1,23 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pinkod;
 
 import java.util.Scanner;
 
-/**
- *
- * @author BertaZoéAnna(SZF_202
- */
 public class PinKod {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         String kod = beker();
-        ellenorzes( kod);
+        ellenorzes(kod);
         keveres();
     }
 
@@ -27,7 +16,7 @@ public class PinKod {
         Scanner sc = new Scanner(System.in);
         String pin = sc.nextLine();
 
-        boolean jo = pin.length() >= 4 && pin.length()<=6;
+        boolean jo = pin.length() >= 4 && pin.length() <= 6;
         if (!jo) {
             System.out.println("nem jó formátum,minimum 4, maximum6 számjegy!");
             beker();
@@ -43,22 +32,22 @@ public class PinKod {
         Scanner sc = new Scanner(System.in);
         String ujPin = sc.nextLine();
         int probalkozasok = 0;
-        while(!ujPin.equals(regi) && probalkozasok<3){
+        while (!ujPin.equals(regi) && probalkozasok < 3) {
             probalkozasok++;
-            System.out.print("Rossz kód! "+"("+probalkozasok+"/3):");
-             ujPin = sc.nextLine();
+            System.out.print("Rossz kód! " + "(" + probalkozasok + "/3):");
+            ujPin = sc.nextLine();
         }
-        if (probalkozasok <3) {
+        if (probalkozasok < 3) {
             System.out.print("Sikeres belépés!");
-        }else{
+        } else {
             System.out.print("belépés megtagadva!");
         }
-        
+
     }
 
     private static void keveres() {
-        int[] tomb = {0,1,2,3,4,5,6,7,8,9};
-        
+        int[] tomb = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     }
 
 }
